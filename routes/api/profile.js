b309c77615a6a7fb1022eb30f5ffacefcf6176bd
moshiurcse <mdbcorporationbd@gmail.com>
2019-09-16 12:42:23 +0600

    Delete profile experiences

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -213,7 +213,7 @@ router.put(
 
     try {
       const profile = await Profile.findOne({ user: req.user.id });
-      profile.experience.ushift(newExp);
+      profile.experience.unshift(newExp);
       profile.save();
 
       res.json(profile);
@@ -223,5 +223,27 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
